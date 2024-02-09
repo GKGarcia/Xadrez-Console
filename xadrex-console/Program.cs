2a7461f843318c17ba3d4ae3e3c3c8281d2d2565
GKGarcia <gabrielkrutsch963@gmail.com>
2024-02-08 22:35:35 -0300

    melhorando a implementacao do metodo imprimir tabuleiro

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -7,10 +7,20 @@ namespace xadrex_console
     {
         static void Main(string[] args) {
 
-           PosicaoXadrez pos = new PosicaoXadrez('a',1);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.colocarpeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarpeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarpeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarpeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.ImprimirTabuleiro(tab);
+
+            }catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
--- xadrex-console/xadrez/PosicaoXadrez.cs
+++ xadrex-console/xadrez/PosicaoXadrez.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using tabuleiro;
 
 namespace xadrez
 {
@@ -14,7 +14,10 @@ namespace xadrez
             this.coluna = coluna;
             this.linha = linha;
         }
-
+        public Posicao ToPosicao()
+        {
+            return new Posicao (8 - linha, coluna - 'a');
+        }
         public override string ToString()
         {
             return "" + coluna + linha;
